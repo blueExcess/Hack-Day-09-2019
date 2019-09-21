89eb4eb4ec631c599ce7e3621f27a88a1aa751d4
Nate Petersen <nate.petersen@me.com>
2019-09-21 00:16:00 -0400

    Add win_condition function
    add function that will copy board array and replace markers and check against all win conditions - will do once for each X and O. Try to find a better way to check. Also add small win function that notifies player and exits script.

--- tictactoe.pl
+++ tictactoe.pl
@@ -29,6 +29,7 @@ chomp $choice;
 verify_entry($choice);
 go_x($choice);
 print_board();
+win_condition(); # test
 # fourth move
 print "Player 2, your move.\n";
 $choice = <STDIN>;
@@ -36,35 +37,72 @@ chomp $choice;
 verify_entry($choice);
 go_o($choice);
 print_board();
+win_condition();
+# fifth move
 print "Player 1, your move.\n";
 $choice = <STDIN>;
 chomp $choice;
 verify_entry($choice);
 go_x($choice);
 print_board();
-print "Player 1 has WON!!!!!\n";
+win_condition();
+# sixth move
+print "Player 2, your move.\n";
+$choice = <STDIN>;
+chomp $choice;
+verify_entry($choice);
+go_x($choice);
+print_board();
+win_condition();
+# seventh move
+print "Player 1, your move.\n";
+$choice = <STDIN>;
+chomp $choice;
+verify_entry($choice);
+go_x($choice);
+print_board();
+win_condition();
+# move eight
+print "Player 2, your move.\n";
+$choice = <STDIN>;
+chomp $choice;
+verify_entry($choice);
+go_x($choice);
+print_board();
+win_condition();
+# ninth move
+print "Player 1, your move.\n";
+$choice = <STDIN>;
+chomp $choice;
+verify_entry($choice);
+go_x($choice);
+print_board();
+win_condition();
+print "You Tied. Congratulations?\n";
+#print "Player 1 has WON!!!!!\n";
 
 
 
 sub verify_entry {
-# check that input is valid and move is valid                                                                                                                                              
+# check that input is valid and move is valid
     my $flag = 1;
     while ($flag eq 1) {
-        unless ($choice =~ /^[1-9,.E]$/ ) {
-            print "Not a single number. Try again. I believe in you!\n";
-            $choice = <STDIN>;
-            chomp $choice;
-        } else {
-            if (@board[$choice - 1] eq 'X' || @board[$choice - 1] eq 'O') {
-                print "That space is already taken - do it right and try again.\n";
-                $choice = <STDIN>;
-                chomp $choice;
-                verify_entry();
-            }
-            $flag = 0;
-        }
+	unless ($choice =~ /^[1-9,.E]$/ ) {
+	    print "Not a single number. Try again. I believe in you!\n";
+	    $choice = <STDIN>;
+	    chomp $choice;
+	} else {
+	    if ($board[$choice - 1] eq 'X' || $board[$choice - 1] eq 'O') {
+		print "That space is already taken - do it right and try again.\n";
+		$choice = <STDIN>;
+		chomp $choice;
+		verify_entry();
+	    }
+	    $flag = 0;
+	}
     }
 }
+	
 
 sub go_o {
 # place marker for O
@@ -101,4 +139,66 @@ sub print_board {
     }
     $count = 1;
     print "------\n";
+}
+
+sub win_condition {
+# check array for win condition                                                                                                                                                     
+    my @check = @board;
+    # check X                                                                                                                                                                       
+    foreach(0 .. 8) {
+        if ($check[$_] eq 'X') {
+            $check[$_] = 0;
+        }
+    }
+    if ($check[0] eq 0 && $check[1] eq 0 && $check[2] eq 0) {
+        endgame();
+    } elsif ($check[3] eq 0 && $check[4] eq 0 && $check[5] eq 0) {
+        endgame();
+    } elsif ($check[6] eq 0 && $check[7] eq 0 && $check[8] eq 0) {
+        endgame();
+    } elsif ($check[0] eq 0 && $check[3] eq 0 && $check[6] eq 0) {
+        endgame();
+    } elsif ($check[1] eq 0 && $check[4] eq 0 && $check[7] eq 0) {
+        endgame();
+    } elsif ($check[2] eq 0 && $check[5] eq 0 && $check[8] eq 0) {
+        endgame();
+    } elsif ($check[0] eq 0 && $check[4] eq 0 && $check[8] eq 0) {
+        endgame();
+    } elsif ($check[2] eq 0 && $check[4] eq 0 && $check[6] eq 0) {
+        endgame();
+    }
+
+    #check O                                                                                                                                                                     
+    @check = @board;
+    foreach(0 .. 8) {
+        if ($check[$_] eq 'O') {
+            $check[$_] = 0;
+        }
+    }
+    if ($check[0] eq 0 && $check[1] eq 0 && $check[2] eq 0) {
+        endgame();
+    } elsif ($check[3] eq 0 && $check[4] eq 0 && $check[5] eq 0) {
+        endgame();
+    } elsif ($check[6] eq 0 && $check[7] eq 0 && $check[8] eq 0) {
+        endgame();
+    } elsif ($check[0] eq 0 && $check[3] eq 0 && $check[6] eq 0) {
+        endgame();
+    } elsif ($check[1] eq 0 && $check[4] eq 0 && $check[7] eq 0) {
+        endgame();
+    } elsif ($check[2] eq 0 && $check[5] eq 0 && $check[8] eq 0) {
+        endgame();
+    } elsif ($check[0] eq 0 && $check[4] eq 0 && $check[8] eq 0) {
+        endgame();
+    } elsif ($check[2] eq 0 && $check[4] eq 0 && $check[6] eq 0) {
+        endgame();
+    }
+#    print "Checker: @check\nBoard: @board\n";
+}
+
+sub endgame {
+# print win and exit -- add var for which player won.                                                                                                                               
+    print "========\n";
+    print "YOU WIN!\n";
+    print "========\n";
+    exit();
 }
