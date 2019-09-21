dc28a109f922d8a9719baea3bc6d67f5b0eb8f14
Nate Petersen <nate.petersen@me.com>
2019-09-20 23:23:18 -0400

    Add verification for input.
    Added a function to verify input (must be a single digit, 1 - 9) and a prompt if user does it incorrectly. Will stay in loop until user gives a valid input.

--- tictactoe.pl
+++ tictactoe.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 # Basic Tic-Tac-Toe for Holberton Hack-Day #1
+# Pre-Alpha - still implementing verification and main loop. Don't look at me.
 
 use strict;
 use warnings;
@@ -11,35 +12,55 @@ print "Player 1 is 'X'. Press a number to choose a space to play.\n";
 print_board();
 my $choice = <STDIN>;
 chomp $choice;
+verify_entry($choice);
 go_x($choice);
 print_board();
 # second move
 print "Player 2, your move.\n";
 $choice = <STDIN>;
 chomp $choice;
+verify_entry($choice);
 go_o($choice);
 print_board();
 # third move
 print "Player 1, your move.\n";
 $choice = <STDIN>;
 chomp $choice;
+verify_entry($choice);
 go_x($choice);
 print_board();
 # fourth move
 print "Player 2, your move.\n";
 $choice = <STDIN>;
 chomp $choice;
+verify_entry($choice);
 go_o($choice);
 print_board();
 print "Player 1, your move.\n";
 $choice = <STDIN>;
 chomp $choice;
+verify_entry($choice);
 go_x($choice);
 print_board();
 print "Player 1 has WON!!!!!\n";
 
+sub verify_entry {
+# check array for win condition
+    my $flag = 1;
+    while ($flag eq 1) {
+	unless ($choice =~ /^[1-9,.E]$/ ) {
+	    print "Not a single number. Try again. I believe in you!\n";
+	    $choice = <STDIN>;
+	    chomp $choice;
+	} else {
+	    $flag = 0;
+	}
+    }
+}
+	
 
 sub go_o {
+# place marker for O
     my $move=@_;
     foreach(@board) {
 	if ($choice eq $_) {
@@ -49,6 +70,7 @@ sub go_o {
 }
 
 sub go_x {
+# Place marker for X
     my $move=@_;
     foreach(@board) {
 	if ($choice eq $_) {
@@ -59,6 +81,7 @@ sub go_x {
 
 
 sub print_board {
+# Print board
     my $count = 1;
     print "------\n";
     foreach(@board) {
